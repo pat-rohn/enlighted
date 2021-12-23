92304afdab46832f55dc8a8e2fa3925c6b1349ea
schusterlein <schusterklein@live.com>
2021-12-23 18:40:35 +0100

    mockup led status

---
+++ src/app/ledstatus-mockup.ts
@@ -0,0 +1,11 @@
+
+import { LEDStatus } from './ledstatus';
+
+const LED_STATUS: LEDStatus = {
+  red: 100,
+  green: 70,
+  blue: 15,
+  brightness: 35,
+  mode: "on",
+  message: "Not Connected",
+}
