e703f9a9a2e5e2e6ea5ac0772bc8f4c362a6b71f
schusterlein <schusterklein@live.com>
2021-12-26 16:22:30 +0100

    Removed logic from explore-container since replaced

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { LedcontrolService } from '../ledcontrol.service';
-import { LEDStatus } from '../ledstatus';
 
 @Component({
   selector: 'app-explore-container',
@@ -10,16 +9,11 @@ import { LEDStatus } from '../ledstatus';
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
-  led?: LEDStatus;
 
   constructor(private ledcontrolService: LedcontrolService) {
   }
 
   ngOnInit() {
-    this.ledcontrolService.getLedStatus()
-      .subscribe(ledstatus => this.led = ledstatus);
-
-    console.log("msg async: " + this.led.message);
 
   }
 
